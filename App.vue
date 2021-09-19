8c18ee8aa023c9773f5911ac5acc61a170a0e316
oginator <54210070+oginator@users.noreply.github.com>
2021-09-19 16:25:12 +0200

    Add files via upload

--- App.vue
+++ App.vue
@@ -5,12 +5,20 @@
       <input type="text" v-model="name" placeholder="Enter name..." />
       <input type="text" v-model="number" placeholder="Enter number..." />
       <button type="submit" v-on:click.prevent="addNewUser()">Add</button>
+      <button v-on:click.prevent="saveStorage()">
+        Save All
+      </button>
     </form>
     <hr />
     <ol>
-      <li v-for="(user, index) in users" v-bind:key="index">
-        {{ user.name + " -- " + user.number }}
-        <button v-on:click="saveStorage(user.name, user.number)">Save</button>
+      <li v-for="(contact, index) in contacts" v-bind:key="index">
+        {{ contact.name + " -- " + contact.number }}
+
+        <button v-on:click="removeElement(index)">Remove</button>
+        <hr />
+      </li>
+      <li v-for="(contact, index) in jsonParsed" v-bind:key="index">
+        {{ contact.name + " -- " + contact.number }}
         <button v-on:click="removeElement(index)">Remove</button>
         <hr />
       </li>
@@ -23,10 +31,15 @@ export default {
   name: "App",
   data() {
     return {
-      users: [],
+      contacts: [],
       name: "",
       number: "",
+      jsonParsed: [],
     };
+  },
+  mounted() {
+    this.jsonParsed = JSON.parse(localStorage.getItem("data"));
+    console.log(this.jsonParsed);
   },
   methods: {
     addNewUser() {
@@ -34,15 +47,19 @@ export default {
         name: this.name,
         number: this.number,
       };
-      this.users.push(newUser);
+      this.contacts.push(newUser);
       this.name = "";
       this.number = "";
     },
     removeElement(index) {
-      this.users.splice(index, 1);
+      if (this.contacts.length > 0) {
+        this.contacts.splice(index, 1);
+      } else if (this.jsonParsed.length > 0) {
+        this.jsonParsed.splice(index, 1);
+      }
     },
-    saveStorage(a, b) {
-      localStorage.setItem(a, b);
+    saveStorage() {
+      localStorage.setItem("data", JSON.stringify(this.contacts));
     },
   },
 };
